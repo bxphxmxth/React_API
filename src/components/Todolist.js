ef9f63f71fff651656d247f010a72691b93594a1
Nasila <fushs@hotmail.fr>
2021-06-30 14:56:09 +0200

    api2

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -3,11 +3,30 @@ import React from "react"
 
 class Todolist extends React.Component{
 
+
+    handleInputChange = (event) => {
+        this.setState({
+            inputValue : event.target.value
+        })
+    }
+
+    state = {
+        inputValue: ""
+    }
+
+    handleClick = () =>{
+        let url = "https://jsonplaceholder.typicode.com/todos?userId=";
+        url += this.state.Value;
+        fetch(url)
+        .then(response => response.json())
+        .then(json => console.log(json))
+    }
+
     render(){
         return(
             <div>
 
-                <input type="text"></input>
+                <input type="text" value={this.state.inputValue} onChange={this.handleInputChange}></input>
                 <button onClick={this.handleClick}>Filter</button>
 
             </div>
@@ -16,3 +35,4 @@ class Todolist extends React.Component{
 }
 
 export default Todolist
+
